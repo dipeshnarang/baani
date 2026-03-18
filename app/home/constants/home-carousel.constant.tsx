63c50f56fe00c2b23caa3d9eff96e4e2b63d3867
Dipesh Narang <dipeshnarang@Dipeshs-MacBook-Pro.local>
2026-03-18 23:32:39 +0530

    update home page icons

--- app/home/constants/home-carousel.constant.tsx
+++ app/home/constants/home-carousel.constant.tsx
@@ -67,17 +67,17 @@ export const LANDMARK_SECTION = {
   subheader: ["across urban landscapes"],
   landmarks: [
     {
-      image: "/images/retail.png",
+      image: "/images/retail-new.png",
       title: "Retail",
       description: "Spaces enabling seamless business–customer interaction.",
     },
     {
-      image: "/images/office.png",
+      image: "/images/office-new.png",
       title: "Offices",
       description: "Environments that ignite productivity and elevate success.",
     },
     {
-      image: "/images/hotel.png",
+      image: "/images/hotel-new.png",
       title: "Hotels",
       description:
         "Properties meticulously crafted to enrich every moment of your stay.",
